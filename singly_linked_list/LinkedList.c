dc5305e85b5e2ca0ef57d19bd27e55c07d95d396
莊雲凱 <as23041248@gmail.com>
2016-05-07 04:42:14 +0800

    write makefile and test

---
+++ singly_linked_list/LinkedList.c
@@ -0,0 +1,7 @@
+#include "LinkedList.h"
+
+
+void printHello(){
+
+    printf("%s\n","hello");
+}
---
+++ singly_linked_list/LinkedList.h
@@ -0,0 +1,10 @@
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+#include <stdlib.h>
+#include <stdio.h>
+
+void printHello();
+
+
+#endif
