4bc652fd9305d75357b47d20f78e5fce640d45f9
莊雲凱 <as23041248@gmail.com>
2016-05-08 02:00:46 +0800

    add singly list spec

--- singly_linked_list/LinkedList.c
+++ singly_linked_list/LinkedList.c
@@ -1,5 +1,16 @@
 #include "LinkedList.h"
 
+void push_front(node **head, int key){}
+int top_front(node **head){}
+void pop_front(node **head){}
+void push_back(node **head, int key){}
+int top_back(node **head){}
+void pop_back(node **head){}
+BOOL find(node **head, int key){}
+void erase(node **head, int key){}
+BOOL empty(node **head){}
+void add_before(node **head, node ** target, int key){}
+void add_after(node **head, node **target, int key){}
 
 void printHello(){
 
--- singly_linked_list/LinkedList.h
+++ singly_linked_list/LinkedList.h
@@ -1,8 +1,30 @@
 
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
+
+
 #include <stdlib.h>
 #include <stdio.h>
+#define BOOL int
+#define TRUE 1
+#define FALSE 0
+
+typedef struct node{
+    int value;
+    struct node *link;
+}node;
+
+void push_front(node **head, int key); //add to front
+int top_front(node **head); //return front item
+void pop_front(node **head); //remove front item
+void push_back(node **head, int key); //add to back
+int top_back(node **head); //return back item
+void pop_back(node **head); //remove back item
+BOOL find(node **head, int key); // is key in list?
+void erase(node **head, int key); // remove key from list
+BOOL empty(node **head); //is list empty?
+void add_before(node **head, node ** target, int key); //add key before target node
+void add_after(node **head, node **target, int key); //add key after target node
 
 void printHello();
 
